f94184fb052e2b4c3f938f2567bff357ab3a598b
Luan Menezes <luancmenezes@gmail.com>
2017-08-12 09:48:55 -0300

    imagens

---
+++ src/pages/satisfacao/satisfacao.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SatisfacaoPage } from './satisfacao';
+
+@NgModule({
+  declarations: [
+    SatisfacaoPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SatisfacaoPage),
+  ],
+})
+export class SatisfacaoPageModule {}
